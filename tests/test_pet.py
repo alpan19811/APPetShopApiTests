63f6ef862a4202fdebcaf7637738d1298a444842
AlekseiNovikov <62709853+alpan19811@users.noreply.github.com>
2025-03-22 21:31:58 +0300

    Добавил тест 'Попытка ополучить информацию о несуществующем питомце'

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -16,8 +16,9 @@ class TestPet:
         with allure.step('Проверка текстового значения ответа'):
             assert response.text == 'Pet deleted', 'Текст ошибки не совпал с ожидаемым'
 
-    @allure.title('Попытка удалить несуществующего питомца')
-    def test_delete_nonexistent_pet(self):
+
+    @allure.title('Попытка обновить несуществующего питомца')
+    def test_update_nonexistent_pet(self):
         with allure.step('Отправка запроса на обновление несуществующего питомца'):
             payload = {
                 "id": 9999,
@@ -31,3 +32,11 @@ class TestPet:
 
             with allure.step("Проверка текстового содержимого ответа"):
                 assert response.text == "Pet not found", 'Текст ошибки не совпал с ожидаемым'
+
+    @allure.title('Попытка получить информацию о несуществующем питомце')
+    def test_get_nonexistent_pet(self):
+        with allure.step('Отправка запроса на получение информации о несуществующем питомце'):
+            response = requests.get(url=f'{BASE_URL}/pet/9999')
+
+        with allure.step('Проверка статуса ответа'):
+            assert response.status_code == 404, 'Код ответа не совпал с ожидаемым'
