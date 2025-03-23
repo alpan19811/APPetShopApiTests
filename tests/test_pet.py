a86f8920f0f1ed0ffc4625529d0b3b76702798ca
AlekseiNovikov <62709853+alpan19811@users.noreply.github.com>
2025-03-23 11:02:03 +0300

    добавил тест- попытка добавить нового питомца

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -15,8 +15,6 @@ class TestPet:
 
         with allure.step('Проверка текстового значения ответа'):
             assert response.text == 'Pet deleted', 'Текст ошибки не совпал с ожидаемым'
-
-
     @allure.title('Попытка обновить несуществующего питомца')
     def test_update_nonexistent_pet(self):
         with allure.step('Отправка запроса на обновление несуществующего питомца'):
@@ -40,3 +38,25 @@ class TestPet:
 
         with allure.step('Проверка статуса ответа'):
             assert response.status_code == 404, 'Код ответа не совпал с ожидаемым'
+
+    @allure.title('Попытка добавить нового питомца')
+    def test_add_new_pet(self):
+        with allure.step('Отправка запроса на добавление нового питомца'):
+            payload = {
+                "id": 1,
+                "name": "Non-existent Buddy",
+                "status": "available"
+            }
+            response = requests.post(f'{BASE_URL}/pet', json=payload)
+
+            with allure.step("Проверка статуса ответа"):
+                assert response.status_code == 200, 'Код ответа не совпал с ожидаемым'
+
+            with allure.step("Проверка данных питомца в ответе"):
+                response_data = response.json()
+                assert response_data['id'] == payload['id'], 'ID питомца не совпал с ожидаемым'
+                assert response_data['name'] == payload['name'], 'Имя питомца не совпало с ожидаемым'
+                assert response_data['status'] == payload['status'], 'Статус питомца не совпал с ожидаемым'
+
+
+
